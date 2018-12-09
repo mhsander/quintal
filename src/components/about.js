a02bc79ab02a21067ee56106d06ecc704e87eb4a
mhsander <martha.sander@airbnb.com>
2018-12-09 10:30:24 -0800

    add about us page, style contact map

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,73 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <h1>Quem Somos</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="about-grid" />
+
+        <div className="about-text">
+          <h2>Quem é o Quintal Urbano?</h2>
+          <hr />
+          <p>
+            O Quintal Urbano surgiu através das vivencias e desejos de dois
+            biólogos, apaixonados pela vida, e amantes da natureza, que
+            resolveram estender suas salas de aulas para além do currículo
+            tradicional.
+            <br />
+            <br />
+            Nossas viagens, nossos estudos, nossas vivencias do dia a dia, foram
+            traçando nossos caminhos, até a ideia de manter a biologia de
+            maneira permanente em nossas vidas. Para isto, começamos a visitar
+            feiras agroecológicas, conhecer as histórias de vida de agricultores
+            familiares, o que nos fez entender a importância deles, para que o
+            alimento chegue a nossa mesa e, principalmente a importância
+            ambiental que desempenham na cadeia produtiva.
+            <br />
+            <br />
+            Hoje, acreditamos em ações educativas, orientadas para conservação
+            ambiental, através do comércio de produtos naturais, orgânicos e
+            veganos. Pois o uso de produtos que são naturais, sem incremento de
+            insumos artificiais e ou, pesticidas mantém o ambiente saudável e em
+            equilíbrio, assim como, a saúde do agricultor e de quem os consome.
+            <br />
+            <br />
+            Deste modo, o Quintal Urbano passa a ser um novo espaço para
+            integrar pessoas, que tem o mesmo objetivo que o nosso, de conservar
+            o ambiente, manter a saúde, e compartilhar estas relações e
+            informações com outras pessoas. Portanto, não focamos somente em
+            vendas, e sim, criamos mais um espaço de compartilhamento de
+            vivencias, de trocas de saberes, onde você possa se sentir bem, e
+            que esse compartilhar do dia a dia, contribua para melhoraria da
+            nossa Terra Mãe.
+          </p>
+          <h2>Visão</h2>
+          <hr />
+          <br />
+          <p />
+          <ul>
+            <li>
+              Temos como base 4 pilares de sustentabilidade, com produtos e
+              práticas:
+            </li>{" "}
+            <br />
+            <li>Ecologicamente corretos</li>
+            <li> Economicamente viáveis</li>
+            <li> Socialmente justos</li>
+            <li> Culturalmente diversos</li>
+            <br />
+          </ul>
+          <br />
+          <h2>Missão</h2>
+          <hr />
+          <p style={{ textAlign: "center" }}>
+            A nossa missão é fornecer produtos de alta qualidade e com preço
+            justo, que respeitem a vida durante sua produção, para pessoas
+            antenadas com a sua saúde e com práticas sustentáveis.
+          </p>
+          <br />
+        </div>
       </div>
     );
   }
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,27 +1,43 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
-import { GoogleApiWrapper, Map, InfoWindow, Marker } from "google-maps-react";
-// import {
-//   withScriptjs,
-//   withGoogleMap,
-//   GoogleMap,
-//   Marker
-// } from "react-google-maps";
 
 class Contact extends Component {
   render() {
     return (
       <div style={{ width: "100%", margin: "auto", paddingTop: "2em" }}>
         <Grid className="landing-grid">
-          <Cell col={6}>
+          <Cell col={12}>
             <h2 style={{ color: "#2BB454" }}>Esperamos a sua visita!</h2>
+            <br />
             <div className="banner-text">
-              <hr />
-              <p> Rua Medianeira 83, São Leopoldo, RS</p>
-              <p> 51 98924-2085 - ligação e WhatsApp</p>
+              <br />
+              <a
+                style={{ color: "#ffffff", fontSize: 25 }}
+                href="https://www.google.com/maps?ll=-29.779309,-51.14835&z=16&t=m&hl=en-US&gl=US&mapclient=embed&daddr=R.+Medianeira,+83+-+Cristo+Rei+S%C3%A3o+Leopoldo+-+RS+93020-410+Brazil@-29.7793093,-51.1483503"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {" "}
+                Rua Medianeira 83, São Leopoldo, RS
+              </a>
+              <br />
+              <br /> <br />
+              <a
+                style={{ color: "#ffffff", fontSize: 25 }}
+                href="https://api.whatsapp.com/send?phone=555189242085&text=&source=&data="
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {" "}
+                51 98924-2085 - ligação e WhatsApp
+              </a>
+              <br />
+              <br />
               <p>quintalurbanosl@gmail.com</p>
+              <br />
             </div>
             <h2 style={{ color: "#2BB454" }}>Horário de funcionamento:</h2>
+            <br />
             <div className="banner-text">
               <p> Segunda à Sexta: 09:30 às 19:30</p>
               <p> Sábado: 9:30 às 16:30</p>
@@ -61,29 +77,8 @@ class Contact extends Component {
               </div>
             </div>
           </Cell>
-          <Cell col={6}>
+          <Cell col={12}>
             <div className="map">
-              {/* <Map
-                google={this.props.google}
-                styles={{
-                  height: "100%",
-                  margin: "0",
-                  padding: "0"
-                }}
-                initialCenter={{
-                  lat: -29.779104,
-                  lng: -51.148403
-                }}
-                zoom={16}
-              >
-                <Marker
-                  onClick={this.onMarkerClick}
-                  title={"Quintal Urbano"}
-                  name={"Quintal Urbano"}
-                />
-
-                <InfoWindow onClose={this.onInfoWindowClose} />
-              </Map> */}
               <iframe
                 title="Mapa"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3462.93966565675!2d-51.15051278489104!3d-29.779386081979517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951968f7f4c70a7f%3A0x70d537c8928c14d0!2sR.+Medianeira%2C+83+-+Cristo+Rei%2C+S%C3%A3o+Leopoldo+-+RS%2C+Brasil!5e0!3m2!1spt-BR!2sus!4v1516669498376"
@@ -100,6 +95,4 @@ class Contact extends Component {
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyD0TgD9PApBv2xhejfMcCvxayoH_d4kTuM"
-})(Contact);
+export default Contact;
