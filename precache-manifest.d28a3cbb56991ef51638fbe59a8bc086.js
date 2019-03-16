e72ab43fa5f5708539217a38dadba1f5c8a382f9
Martha Sander <martha.sander@airbnb.com>
2019-03-16 16:29:38 -0700

    Updates

---
+++ precache-manifest.d28a3cbb56991ef51638fbe59a8bc086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc1c95d4a2c92afe7c6f",
+    "url": "/quintal/static/js/runtime~main.2ea5db60.js"
+  },
+  {
+    "revision": "2034496432c3e6d94b6f",
+    "url": "/quintal/static/js/main.136d33d8.chunk.js"
+  },
+  {
+    "revision": "526eb934cf1d51e9ecc9",
+    "url": "/quintal/static/js/2.168b49e8.chunk.js"
+  },
+  {
+    "revision": "2034496432c3e6d94b6f",
+    "url": "/quintal/static/css/main.378db7d3.chunk.css"
+  },
+  {
+    "revision": "526eb934cf1d51e9ecc9",
+    "url": "/quintal/static/css/2.7868eb4f.chunk.css"
+  },
+  {
+    "revision": "ca3e41ddbdf2f0437183aace23a97581",
+    "url": "/quintal/index.html"
+  }
+];
